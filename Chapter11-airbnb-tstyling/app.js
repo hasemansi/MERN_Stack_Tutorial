78bbd71fda886d260969ced6414a23b8b2d0a148
hasemansi <hasemansi233@gmail.com>
2026-09-22 22:41:42 +0530

    styling airbnb using tailwindcss

---
+++ Chapter11-airbnb-tstyling/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const userRouter = require('./routes/userRouter');
+const hostRouter = require('./routes/hostRouter');
+const path = require('path');
+const rootDir = require('./utils/pathUtil');
+
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.urlencoded());
+app.use('/',userRouter);
+app.use('/host',hostRouter);
+
+app.use(express.static(path.join(rootDir, 'public')));
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
